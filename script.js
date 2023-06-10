77827b755ca8501d1070c85d511073a1720febab
ashton8504 <ashtonlittle8504@gmail.com>
2023-06-10 08:49:30 -0400

    update

--- script.js
+++ script.js
@@ -109,3 +109,18 @@ searchInput.addEventListener('keypress', function (event) {
     performMovieSearch();
   }
 });
+
+
+
+// Script to run carousel
+$(document).ready(function() {
+    $(".carousel").owlCarousel({
+      items: 1,
+      loop: true,
+      autoplay: true,
+      autoplayTimeout: 5000,
+      autoplayHoverPause: true,
+      nav: false,
+      dots: true
+    });
+  });
